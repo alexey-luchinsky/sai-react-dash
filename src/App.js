72529aad56bb8b17ee424f06fcfa10f6dfeec4cd
Aleksei Luchinskii <aluchi@bgsu.edu>
2021-06-18 08:33:56 -0400

    Simmple RGL dashboard is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './css/App.css';
-
+import SAI_Dashboard from "./SAI_Dashboard"
+import "./css/react-grid-layout_styles.css"
 function App() {
   return (
     <div className="App">
       <h1> SAI Dashboard</h1>
+      <SAI_Dashboard/>
     </div>
   );
 }
