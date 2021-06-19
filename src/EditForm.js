cc81a5ce6e3ffbe85f91ac6c518a2d3361fda48c
Aleksei Luchinskii <aluchi@bgsu.edu>
2021-06-19 14:19:25 -0400

    empty EditTextForm

--- src/EditForm.js
+++ src/EditForm.js
@@ -1,13 +1,30 @@
 import { throwStatement } from '@babel/types';
 import React from 'react';
 import ReactModal from 'react-modal';
-import { SaiDash
- } from './SaiDash';
+import {SaiDash} from './SaiDash';
+
+class EditTextForm extends React.Component {
+    render() {
+        return <div>Edit Text</div>;
+    }
+}
+
+
 export default class EditForm extends React.Component {
     render() {
-        let preview = <div></div>;
-        if(this.props.element) {
-            preview = <SaiDash type={this.props.element.type} data={this.props.element.data}/>;
+        let element = this.props.element;
+        let preview = <div>Element is empty</div>;
+        let editPane = <div>Element is empty</div>
+        if(element) {
+            preview = <SaiDash type={element.type} data={element.data}/>;
+            if(element.type === "text") {
+                console.log("edit text");
+                editPane = <EditTextForm/>
+            }
+            else {
+                editPane = <div>The element type {this.props.element.type} cannot be edited</div>;
+            }
+
         };
         return(
             <ReactModal 
@@ -17,6 +34,7 @@ export default class EditForm extends React.Component {
                  <div className="row">
                     <div className="column" style={{float:"left",width:"50%"}}>
                         <button onClick={this.props.closeEditForm}>Close</button>
+                        {editPane}
                     </div>
                     <div className="column" style={{float:"right",width:"50%"}}>
                         <button onClick={() => {
